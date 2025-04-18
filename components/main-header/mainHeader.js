16393beafe2e25c12f50711d705b01447a5bc256
Nikhil <nikhil@Nikhils-MacBook-Air.local>
2025-04-18 16:15:52 +0530

    commit

--- components/main-header/mainHeader.js
+++ components/main-header/mainHeader.js
@@ -12,7 +12,7 @@ export default function MainHeader() {
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
           <Image src={logoImg} alt="A plate with food on it" priority />
-          NextLevel Food
+          Foodies
         </Link>
 
         <nav className={classes.nav}>
